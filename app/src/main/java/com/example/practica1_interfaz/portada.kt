97c9efee2d387889227cc7f5dd63f991c9b33a06
guicamher <guillecampher15@gmail.com>
2025-09-17 15:55:12 +0200

    commit n2

--- app/src/main/java/com/example/practica1_interfaz/portada.kt
+++ app/src/main/java/com/example/practica1_interfaz/portada.kt
@@ -1,7 +1,10 @@
 package com.example.practica1_interfaz
 
+import android.R
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -9,28 +12,94 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.practica1_interfaz.ui.theme.azulesB
+import com.example.practica1_interfaz.ui.theme.negro
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            hori(modifier)
+        }
+        else -> {
+            verti(modifier)
+        }
+    }
+}
+@Composable
+fun hori(modifier: Modifier){
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(-50.dp),
         modifier = modifier.fillMaxSize().wrapContentHeight()
-        ){
+    ){
         Text(
             text = "Play Juegos",
             modifier = modifier,
-            color = Color.Green,
+            color = negro,
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic
+        )
+        Spacer(modifier = modifier.size(50.dp))
+
+        Row {
+            Button(onClick = { }, modifier = modifier.width(200.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+            ) {
+                Text("Play")
+            }
+            Spacer(modifier = modifier.size(10.dp))
+
+            Button(onClick = { }, modifier = modifier.width(200.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+            ) {
+                Text("New Player")
+            }
+        }
+
+        Row {
+            Button(onClick = {  }, modifier = modifier.width(200.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+            ) {
+                Text("Preferences")
+            }
+            Spacer(modifier = modifier.size(10.dp))
+
+            Button(onClick = {  }, modifier = modifier.width(200.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+            ) {
+                Text("About")
+            }
+        }
+
+
+    }
+}
+@Composable
+fun verti(modifier: Modifier){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(-50.dp),
+        modifier = modifier.fillMaxSize().wrapContentHeight()
+    ){
+        Text(
+            text = "Play Juegos",
+            modifier = modifier,
+            color = negro,
             fontSize = 50.sp,
             fontWeight = FontWeight.Bold,
             fontStyle = FontStyle.Italic
@@ -38,22 +107,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Spacer(modifier = modifier.size(50.dp))
 
 
-        Button(onClick = { }, modifier = modifier.width(200.dp)) {
+        Button(onClick = { }, modifier = modifier.width(200.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+        ) {
             Text("Play")
         }
-        Button(onClick = { }, modifier = modifier.width(200.dp)) {
+        Button(onClick = { }, modifier = modifier.width(200.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+        ) {
             Text("New Player")
         }
-        Button(onClick = {  }, modifier = modifier.width(200.dp)) {
+        Button(onClick = {  }, modifier = modifier.width(200.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+        ) {
             Text("Preferences")
         }
-        Button(onClick = {  }, modifier = modifier.width(200.dp)) {
+        Button(onClick = {  }, modifier = modifier.width(200.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = azulesB)
+        ) {
             Text("About")
         }
-
-
-
-
     }
 }
 
--- app/src/main/java/com/example/practica1_interfaz/ui/theme/Color.kt
+++ app/src/main/java/com/example/practica1_interfaz/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val verdes = Color(0xFFABDE96)
+
+val azulesB = Color(0xFF3B9C8F)
+
+val negro = Color(0xFF282B26)
