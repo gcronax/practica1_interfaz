746c14e51c7b6cf3f1e133f0aff98a8c7a15acae
gcronax <guillecampher15@gmail.com>
2025-10-14 13:56:51 +0200

    a

--- app/src/main/java/com/example/practica1_interfaz/splash.kt
+++ app/src/main/java/com/example/practica1_interfaz/splash.kt
@@ -1,8 +1,11 @@
 package com.example.practica1_interfaz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -14,13 +17,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
 
 @Composable
 fun splash(navController: NavHostController, modifier: Modifier = Modifier) {
 
-    Text(text = "prueba")
+    Box(
+        modifier.fillMaxSize()
+    ){
+        Text(text = "prueba")
+        Image(
+            painter = painterResource(id = R.drawable.screen),
+            contentDescription = "Logotipo de la Aplicación",
+            modifier = Modifier.requiredSize(300.dp).align(Alignment.Center)
+        )
+    }
+
     val delaySeconds: Long = 2
     LaunchedEffect(true) {
         delay(delaySeconds * 1000)
