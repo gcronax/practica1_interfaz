213ba6c6fb552a18b5f1d11bf911cb3d359987b4
guicamher <guillecampher15@gmail.com>
2025-10-20 20:25:27 +0200

    commit n10

--- app/src/main/java/com/example/practica1_interfaz/about.kt
+++ app/src/main/java/com/example/practica1_interfaz/about.kt
@@ -1,9 +1,17 @@
 package com.example.practica1_interfaz
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,7 +20,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -27,7 +38,7 @@ data class imgenes(
 @Composable
 fun about(navController: NavHostController, modifier: Modifier = Modifier) {
 
-    val personaSeleccionada by remember {
+    val imagenSeleccionada by remember {
         mutableStateOf(listOf(
             imgenes(
                 "María Mata",
@@ -71,12 +82,41 @@ fun about(navController: NavHostController, modifier: Modifier = Modifier) {
             )
         ))
     }
-
-
-    Box(
-        modifier.fillMaxSize()
-    ){
-
-
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        items(imagenSeleccionada.size) { index ->
+            rows(imagenSeleccionada[index])
+        }
     }
+
+}
+
+@Composable
+fun rows(imagen: imgenes){
+    Row ( horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically)
+    {
+        Image(
+            contentScale = ContentScale.Crop,
+            painter = painterResource(id = imagen.direccion),
+            contentDescription = "Imagen por defecto",
+            modifier = Modifier
+                .size(100.dp)
+                .padding(10.dp)
+                .clip(RoundedCornerShape(40.dp))
+        )
+
+        Column {
+            Text(
+                text = imagen.nombre,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Puntos: ${imagen.puntuacion}"
+            )
+        }
+    }
+    HorizontalDivider()
 }
